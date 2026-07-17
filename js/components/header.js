09b1e2ea82b0bc9fa797b8f442f7558d64bf850b
Therese C Nordnes <therese@getacademy.no>
2026-07-17 14:57:18 +0200

    setup

---
+++ js/components/header.js
@@ -0,0 +1,10 @@
+export default function Header () {
+    return /*HTML*/`
+    <div>
+      
+        <img src="../assets/icons/cartify_mobile_icon.png">
+        <a href="../account/login.html">Log in  </a>
+        <a href="../cart/index.html">cart  </a>
+    </div>
+    `
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import Header from "./components/header.js";
+
+
+
+
+document.getElementById("header").innerHTML = Header();
